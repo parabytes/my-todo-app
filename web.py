def0ba0e5fe2d1f49d495bf63c045d9dab4738ff
parabytes <61739760+parabytes@users.noreply.github.com>
2023-06-24 12:24:42 -0400

    Second Commit

--- web.py
+++ web.py
@@ -20,8 +20,12 @@ st.title("My todo App")
 st.subheader("This is my todo app")
 st.write("This app is to increase your <b>productivity</b>.", unsafe_allow_html=True)
 
-for todo in todos:
-	st.checkbox(todo, key=todo)
+for index, todo in enumerate(todos):
+	checkbox = st.checkbox(todo, key=todo)
+	if checkbox:
+		todos.pop(index)
+		cli.write_todos(todos)
+		del st.session_state[todo]
 
 if st.checkbox:
 	print(st.checkbox)
